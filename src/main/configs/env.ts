939e2c4172b139de09e01695140e96b660368a8a
Lucas Lima <lukenoutte@gmail.com>
2024-09-12 17:27:16 -0300

    fix/envs

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -4,5 +4,6 @@ dotenv.config({ path: ".env" });
 
 const postgreUrl = process.env.DATABASE_URL as string;
 const serverPort = process.env.PORT || 3003;
+const appUrl = process.env.APP_URL || "http://localhost:3000";
 
-export { serverPort, postgreUrl };
+export { serverPort, postgreUrl, appUrl };
